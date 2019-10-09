d0a98b2b9207e39b4904af50e6775792072d3b2b
sushma143 <sushmasinghjk@gmail.com>
2019-10-09 19:31:28 +0530

    SetterInjection added

---
+++ SetterInjection/src/main/java/com/zensar/spring/main/ZensarIdolMain.java
@@ -0,0 +1,23 @@
+package com.zensar.spring.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.zensar.spring.instruments.Instrument;
+import com.zensar.spring.performers.Performer;
+
+public class ZensarIdolMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext ctx=new ClassPathXmlApplicationContext("performers.xml");
+		Performer p=ctx.getBean("aahana",Performer.class);
+		p.perform();
+		Instrument p1=ctx.getBean("guitar",Instrument.class);
+		p1.play();
+	
+		
+
+	}
+
+}
